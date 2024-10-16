7a943385e6175a6466f6788a1cb413fd3ab7818d
vikram3456 <ravi@techouts.com>
2024-10-16 15:08:51 +0530

    added backed to the post listing and useEffect hook

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,15 +1,43 @@
 import classes from './NewPost.module.css';
+import { useState } from "react";
+function NewPost({ onCancel, onAddPost }) {
 
-function NewPost() {
+  const [submitedValue, updateSubmitedValue] = useState({ name: '', body: '' })
+
+
+  function changeBodyHandler(event) {
+    updateSubmitedValue((prevState) => {
+      return { ...prevState, body: event.target.value }
+    })
+  }
+  function changeNameHandler(event) {
+    updateSubmitedValue((prevState) => {
+      return { ...prevState, name: event.target.value }
+    })
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      name: submitedValue.name,
+      body: submitedValue.body
+    }
+    onAddPost(postData)
+    onCancel()
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit = {submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={changeNameHandler} />
+      </p>
+      <p className={classes.actions}>
+        <button type='button' onClick={onCancel}>Cancel</button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,34 +1,50 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import Model from "./Model";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./PostsList.module.css"
-function PostList({onStopPosting,isPosting}) {
-  const [submitedvalue, updateSubmitedValue] = useState({name:'',body:''})
 
-
-function changeBodyHandler (event){
-  updateSubmitedValue((prevState)=>{
-    return {...prevState,body: event.target.value}
-  })
-}
-function changeNameHandler (event){
-  updateSubmitedValue((prevState)=>{
-    return {...prevState,name:event.target.value}
-  })
-}
-
+function PostList({ onStopPosting, isPosting }) {
+  const [posts, setPosts] = useState([])
+  const [isFetching, setIsFetching] = useState()
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true)
+      let response = await fetch('http://localhost:8080/posts')
+      let resPost = await response.json()
+      setPosts(resPost.posts)
+      setIsFetching(false)
+    }
+    fetchPosts();
+  }, [])
+  function addingPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setPosts((existingPosts) => [...existingPosts, postData])
+  }
   return (
     <>
-    {isPosting && <Model onClose = {onStopPosting}>
-    <NewPost 
-    onBodyChange = {changeBodyHandler} 
-    onNameChange = {changeNameHandler}/>
-    </Model>}
-    <ul className={classes.posts}>
-    <Post author={submitedvalue.name} body={submitedvalue.body}/>
-    <Post author="vikram" body="this is test 2"/>
-    </ul>
+      {isPosting && <Model onClose={onStopPosting}>
+        <NewPost
+          onCancel={onStopPosting}
+          onAddPost={addingPostHandler} />
+      </Model>}
+      {(posts.length > 0 && !isFetching) &&
+        <ul className={classes.posts}>
+          {posts.map((post) => <Post key={post.name} author={post.name} body={post.body} />)}
+        </ul>}
+      {(posts.length === 0 && !isFetching) &&
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>no posts to show</p>
+        </div>}
+      {isFetching && <div style={{ textAlign: 'center', color: 'white' }}>
+        <p>... loading posts</p>
+      </div>}
     </>
   );
 }
